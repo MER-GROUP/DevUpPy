f9a1e2fc363e03c59b1e980b453f763c3d25db53
Red Alert aka Max Ramanenka <mer-group@yandex.ru>
2022-02-06 22:47:17 +0300

    dup v1.1

--- dup.py
+++ dup.py
@@ -3,9 +3,9 @@ import pip
 
 class DAP:
     def __init__(self) -> None:
-        self.__update_py_pkg()
+        self.__upgrade_pkg_py()
 
-    def __update_py_pkg(self):
+    def __upgrade_pkg_py(self):
         pkgs = eval(str(sbp.run("pip3 list -o --format=json", shell=True,
                          stdout=sbp.PIPE).stdout, encoding='utf-8'))
         for pkg in pkgs:
